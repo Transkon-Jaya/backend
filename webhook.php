870fab1c09b3bcee30abd7ea807b02d5edd2ce06
Aminemc <aminemc236@gmail.com>
2025-04-10 11:39:49 +0800

    webhook

--- webhook.php
+++ webhook.php
@@ -1,13 +1,13 @@
 <?php
-// file_put_contents('webhook.log', date('Y-m-d H:i:s') . " - Webhook received\n", FILE_APPEND);
 
-// if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-//     http_response_code(403);
-//     echo "Forbidden";
-//     exit;
-// }
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(403);
+    echo "Forbidden";
+    exit;
+}
 
-echo shell_exec('sudo -u corcomm git reset --hard HEAD && sudo -u corcomm git pull');
+shell_exec('sudo -u corcomm git reset --hard HEAD');
+echo shell_exec('sudo -u corcomm git pull');
 // file_put_contents('webhook.log', $output . "\n", FILE_APPEND);
 // echo $output;
 // echo "Webhook executed\n";
