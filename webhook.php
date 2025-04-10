b7d803c8682c580a3f5bc9700e09c0641822189a
Aminemc <aminemc236@gmail.com>
2025-04-10 11:24:04 +0800

    webhook

--- webhook.php
+++ webhook.php
@@ -7,7 +7,7 @@
 //     exit;
 // }
 
-echo shell_exec('git reset --hard HEAD && git pull');
+echo shell_exec('sudo -u corcomm git reset --hard HEAD && git pull');
 // file_put_contents('webhook.log', $output . "\n", FILE_APPEND);
 // echo $output;
 // echo "Webhook executed\n";
