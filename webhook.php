03412e4af49fe03366c12a9ec510f1a2df15b977
Aminemc <aminemc236@gmail.com>
2025-04-10 11:34:01 +0800

    webhook

--- webhook.php
+++ webhook.php
@@ -7,7 +7,7 @@
 //     exit;
 // }
 
-echo shell_exec('sudo -u corcomm git pull > webhook.log 2>&1');
+echo shell_exec('sudo -u corcomm git pull > webhook.log');
 // file_put_contents('webhook.log', $output . "\n", FILE_APPEND);
 // echo $output;
 // echo "Webhook executed\n";
