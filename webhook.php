807dbfe54cac1cfdb575607b3d61157c7a4a07c1
Aminemc <aminemc236@gmail.com>
2025-04-10 11:03:50 +0800

    webhook

--- webhook.php
+++ webhook.php
@@ -1,9 +1,9 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(403);
-    echo "Forbidden";
-    exit;
-}
+// if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+//     http_response_code(403);
+//     echo "Forbidden";
+//     exit;
+// }
 // $output = shell_exec('cd /var/www/html/api && git reset --hard HEAD && git pull 2>&1');
 shell_exec('git pull');
 echo "Webhook executed\n";
