fca57dccf093a2580351a2a91297251255c697ed
Aminemc <aminemc236@gmail.com>
2025-04-28 09:53:42 +0800

    test expirty

--- login.php
+++ login.php
@@ -60,7 +60,7 @@ if ($user) {
 
     if (password_verify($password, $user['passwd'])) {
         $issued_at = time();
-        $expiration_time = $issued_at + (10); // Token expires in 1 day
+        $expiration_time = $issued_at + (60 * 60 * 12); // Token expires in 1 day
         $payload = [
             "username" => $username,
             "photo" => $photo,
