e8c4b43f07e0d07a46272bfa21db1176129195a4
Mukarabin Ali Amu <129189189+mukarabin@users.noreply.github.com>
2025-05-17 09:06:37 +0800

    Update table-user.php

--- services/hr/table-user.php
+++ services/hr/table-user.php
@@ -13,7 +13,11 @@ switch ($method) {
         }
 
         $username = $conn->real_escape_string($_GET['username']);
-        $sql = "SELECT tanggal, hour_in, hour_out FROM hr_absensi WHERE username = '$username' ORDER BY id DESC LIMIT 1";
+        $sql = "SELECT tanggal, hour_in, hour_out 
+                    FROM hr_absensi 
+                    WHERE username = '$username' 
+                    AND tanggal >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+                    ORDER BY tanggal DESC;";
         $result = $conn->query($sql);
         if (!$result) {
             http_response_code(500);
@@ -34,13 +38,6 @@ switch ($method) {
         echo json_encode(["status" => 405, "error" => "Invalid request method"]);
         break;
 }
-if (!$result) {
-    error_log("SQL Error: " . $conn->error); // tambahkan ini
-    http_response_code(500);
-    echo json_encode(["status" => 500, "error" => $conn->error]);
-    break;
-}
-
 
 $conn->close();
 ?>
