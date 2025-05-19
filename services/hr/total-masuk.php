d2b5f085c50f54ca460ea4b3c47e61db9c1f05de
Mukarabin Ali Amu <129189189+mukarabin@users.noreply.github.com>
2025-05-19 10:43:35 +0800

    total masuk

---
+++ services/hr/total-masuk.php
@@ -0,0 +1,42 @@
+<?php
+header("Content-Type: application/json");
+require 'db.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case 'GET':
+        if (!isset($_GET['username'])) {
+            http_response_code(400);
+            echo json_encode(["status" => 400, "error" => "No Username!"]);
+            break;
+        }
+
+        $username = $conn->real_escape_string($_GET['username']);
+        $sql = "SELECT COUNT(DISTINCT username) AS total_masuk
+                FROM hr_absensi
+                WHERE tanggal = CURDATE()
+                AND (hour_in IS NOT NULL);";
+        $result = $conn->query($sql);
+        if (!$result) {
+            http_response_code(500);
+            echo json_encode(["status" => 500, "error" => $conn->error]);
+            break;
+        }
+
+        $status = [];
+        while ($row = $result->fetch_assoc()) {
+            $status[] = $row;
+        }
+
+        echo json_encode($status);
+        break;
+
+    default:
+        http_response_code(405);
+        echo json_encode(["status" => 405, "error" => "Invalid request method"]);
+        break;
+}
+
+$conn->close();
+?>
