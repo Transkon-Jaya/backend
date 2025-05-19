72a8991a366fa485e0b1bad3ddf6af1605506b0b
aminemcc <aminemc236@gmail.com>
2025-05-19 21:31:24 +0800

    absensi leaderboard

--- services/chart.php
+++ services/chart.php
@@ -51,16 +51,19 @@ $allowed_routes = [
     $prefix.'absensi_leaderboard_hi_desc' => [
         'query' => 'CALL absensi_leaderboard_hi_desc(?, ?, ?, ?)', //start_date, end_date, placement, department
         'params' => 4,
+        'level' => 8,
         'permissions' => ['admin_absensi'],
     ],
     $prefix.'absensi_leaderboard_ho_desc' => [
         'query' => 'CALL absensi_leaderboard_ho_desc(?, ?, ?, ?)', //start_date, end_date, placement, department
         'params' => 4,
+        'level' => 8,
         'permissions' => ['admin_absensi'],
     ],
     $prefix.'absensi_leaderboard_hw_desc' => [
         'query' => 'CALL absensi_leaderboard_hw_desc(?, ?, ?, ?)', //start_date, end_date, placement, department
         'params' => 4,
+        'level' => 8,
         'permissions' => ['admin_absensi'],
     ],
     //--ABSENSI END--//
