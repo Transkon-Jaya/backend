a1fa9aa83d479a8369d501fab0eb01cf9bc79948
aminemcc <aminemc236@gmail.com>
2025-06-10 16:13:52 +0800

    test dynamicQuery

--- services/ip.php
+++ services/ip.php
@@ -4,7 +4,7 @@ require_once 'utils/getClientIP.php';
 require 'auth.php';
 
 $username = $_GET['username'] ?? null;
-authorize(10, [], [], $username);
+// authorize(10, [], [], $username);
 
 $response = [
     'ip' => getClientIP()
