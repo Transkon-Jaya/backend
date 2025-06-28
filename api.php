89d20734e80ecd5f61115b7d634a8c97d4c12864
Mukarabin Ali Amu <129189189+mukarabin@users.noreply.github.com>
2025-06-28 10:01:59 +0800

    Update api.php

--- api.php
+++ api.php
@@ -41,7 +41,7 @@ $allowed_routes = [
     'asset-locations' => 'services/assets/locations.php',
     'asset-stocks'    => 'services/assets/stocks.php',
     'asset-upload'   => 'services/assets/upload.php',
-    'asset-stock-history' => 'services/assets/stock-history.php'
+    'asset-stock-history' => 'services/assets/stock-history.php',
     'asset-update' => 'services/assets/update.php',
 ];
 
