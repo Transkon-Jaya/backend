934d7b2b224c74a17e969cf026637bf7cced59d3
Mukarabin Ali Amu <129189189+mukarabin@users.noreply.github.com>
2025-07-28 15:59:35 +0800

    Update score.php

--- services/score.php
+++ services/score.php
@@ -1,4 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+// Tangani preflight request OPTIONS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Hanya kirim header dan keluar jika ini preflight
+    http_response_code(200);
+    exit();
+}
 // api/score.php
 header("Content-Type: application/json");
 require 'db.php'; // Pastikan path ini benar relatif terhadap score.php
