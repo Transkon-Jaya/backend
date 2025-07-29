f70bc6bdf547def0f296de2cdec9fcd5f84a5341
Mukarabin Ali Amu <129189189+mukarabin@users.noreply.github.com>
2025-07-29 08:37:34 +0800

    Update score.php

--- services/score.php
+++ services/score.php
@@ -1,12 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Authorization, Content-Type");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
 // api/score.php
 header("Content-Type: application/json");
 require 'db.php'; // Pastikan path ini benar relatif terhadap score.php
