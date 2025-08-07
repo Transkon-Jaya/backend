4c30a3c87aa936393479545d6c462be38456cac9
Mukarabin Ali Amu <129189189+mukarabin@users.noreply.github.com>
2025-08-07 11:12:27 +0800

    Update perizinan.php

--- services/hr/perizinan.php
+++ services/hr/perizinan.php
@@ -1,9 +1,5 @@
 <?php
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
