286d56e159ac3322f42391c2c4ebda3c74ee2533
mukarabin <mukarabinaliamu@gmail.com>
2025-08-10 06:35:35 +0800

    Update transmittals.php

--- services/commercial/transmittals.php
+++ services/commercial/transmittals.php
@@ -1,18 +1,5 @@
 <?php
-// Tambahkan CORS
-header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Max-Age: 86400");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-
 header("Content-Type: application/json");
-
 require 'db.php';
 require 'auth.php';
 
