e288bb3d72a74c6f28d250396c534649cf81e1d5
mukarabin <mukarabinaliamu@gmail.com>
2025-08-10 14:57:13 +0800

    Update transmittals.php

--- services/commercial/transmittals.php
+++ services/commercial/transmittals.php
@@ -1,8 +1,8 @@
 <?php
 // Aktifkan error reporting untuk debugging
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('log_errors', 1);
+ini_set('display_errors', 0); // Jangan tampilkan error ke user
+ini_set('log_errors', 1); 
 ini_set('error_log', __DIR__ . '/transmittal_error.log');
 
 header("Content-Type: application/json");
