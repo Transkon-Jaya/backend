85406b95918220ce99765d5bf6b4d24efbd153a1
mukarabin <mukarabinaliamu@gmail.com>
2025-08-10 09:14:10 +0800

    Update transmittals.php

--- services/commercial/transmittals.php
+++ services/commercial/transmittals.php
@@ -1,4 +1,9 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', 'php-error.log');
+
 header("Content-Type: application/json");
 require_once 'db.php';
 require_once 'auth.php';
